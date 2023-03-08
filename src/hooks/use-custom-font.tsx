dff9bb7315c835dd12e866c579675bd0b8f5b9fb
jovinlidan <jovinlidan1@gmail.com>
2023-03-08 10:29:39 +0700

    add custom fonts

---
+++ src/hooks/use-custom-font.tsx
@@ -0,0 +1,87 @@
+import { useFonts } from "expo-font";
+import { useMemo } from "react";
+import { StyleProp, TextStyle, TextProps, StyleSheet } from "react-native";
+
+interface Fonts {
+  fontFamily: string;
+  fontStyles: any;
+  fontWeights: any;
+}
+
+const fonts: Fonts = {
+  fontFamily: "NotoSans",
+  fontWeights: {
+    100: "Thin",
+    200: "Light",
+    300: "Light",
+    400: "Regular",
+    500: "Regular",
+    600: "Regular",
+    700: "Bold",
+    800: "Bold",
+    900: "Bold",
+    thin: "Thin",
+    light: "Light",
+    normal: "Regular",
+    bold: "Bold",
+  },
+  fontStyles: {
+    normal: "",
+    italic: "Italic",
+  },
+};
+
+interface CustomFont {
+  style: StyleProp<TextStyle>;
+  props: TextProps;
+}
+
+export function getFontFamily({
+  fontWeight,
+  fontStyle,
+}: {
+  fontWeight?: string;
+  fontStyle?: string;
+}) {
+  const customFontWeight = fontWeight
+    ? fonts.fontWeights[fontWeight]
+    : "Regular";
+  const customFontStyle = fontStyle ? fonts.fontStyles[fontStyle] : "";
+  const modifier = customFontWeight + customFontStyle;
+  return `${fonts.fontFamily}-${modifier}`;
+}
+
+const useCustomFont = (
+  props: TextProps,
+  defaultStyle: TextStyle
+): CustomFont => {
+  const { style, ...restProps } = props;
+  const customizedStyle = useMemo((): StyleProp<TextStyle> => {
+    const { fontWeight, fontStyle, ...rest } =
+      StyleSheet.flatten(style) || ({} as any);
+    return {
+      ...rest,
+      fontFamily: getFontFamily({ fontWeight, fontStyle }),
+    };
+  }, [style]);
+  const newStyle = StyleSheet.flatten([defaultStyle, customizedStyle]);
+  return {
+    style: newStyle,
+    props: restProps,
+  };
+};
+
+export function useInitiateCustomFont() {
+  return useFonts({
+    "Lato-Thin": require("@app/assets/fonts/Lato-Thin.ttf"),
+    "Lato-ThinItalic": require("@app/assets/fonts/Lato-ThinItalic.ttf"),
+    "Lato-Light": require("@app/assets/fonts/Lato-Light.ttf"),
+    "Lato-LightItalic": require("@app/assets/fonts/Lato-LightItalic.ttf"),
+    "Lato-Regular": require("@app/assets/fonts/Lato-Regular.ttf"),
+    "Lato-RegularItalic": require("@app/assets/fonts/Lato-Italic.ttf"),
+    "Lato-Bold": require("@app/assets/fonts/Lato-Bold.ttf"),
+    "Lato-BoldItalic": require("@app/assets/fonts/Lato-BoldItalic.ttf"),
+  });
+}
+
+export default useCustomFont;
